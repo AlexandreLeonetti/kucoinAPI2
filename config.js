bbc04573c14b677b13dd04d605c8546c3fa95e65
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-04-09 22:13:11 +0200

    del

--- config.js
+++ config.js
@@ -16,15 +16,4 @@ baseUrl: 'https://openapi-v2.kucoin.com',
   },
   authVersion: 2, // KC-API-KEY-VERSION. Notice: for v2 API-KEY, not required for v1 version.
 };
-/*
-module.exports = {
-//  baseUrl: 'https://openapi-sandbox.kucoin.io',
-baseUrl: 'https://openapi-v2.kucoin.com',
-  apiAuth: {
-    key: '661550b904c625000146908d', // KC-API-KEY
-    secret: 'a69b9e5f-c547-4773-b5b3-aa05e6199ef9', // API-Secret
-    passphrase: '348_3HHu__-3jUwiedb____X', // KC-API-PASSPHRASE
-  },
-  authVersion: 2, // KC-API-KEY-VERSION. Notice: for v2 API-KEY, not required for v1 version.
-};
-*/
+
